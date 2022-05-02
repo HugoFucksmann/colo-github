2e91c0e2aabca9d81cf4dfb67675a71895eb69c7
hfucksmann <hugoffuksmann@gmail.com>
2022-05-02 12:08:53 -0300

    add init prop cardimg1

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import { appTheme } from "./theme/appTheme";
 import { ThemeProvider } from "@mui/system";
 import SinglePage from "./colo/singlePage";
+import CardImg1 from "./componets/cards/cardImg1";
 
 function App() {
   return (
     <ThemeProvider theme={appTheme}>
-      <SinglePage />
+      <CardImg1 />
     </ThemeProvider>
   );
 }
