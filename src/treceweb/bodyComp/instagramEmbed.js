5cfa09f70e38f52ebce9d97e23b3a31b4b46a0f2
hfucksmann <hugoffuksmann@gmail.com>
2022-05-05 10:26:59 -0300

    g

--- src/treceweb/bodyComp/instagramEmbed.js
+++ src/treceweb/bodyComp/instagramEmbed.js
@@ -1,11 +1,14 @@
 import InstagramFeed from "react-ig-feed";
 import "react-ig-feed/dist/index.css";
 
-const token =
-  "EAAFrzBNW9DkBAO0F4ou4ER3SCspDNjoUIq2MueFn7pxBFsIBQl902i4aC9T7qAQNpZC8vGqSjAqZBnLno2NFWpuFswnfi3jZC38ZCYB7ZBgwc9q9sZALPAte38e981cK66NwcU7uzBHMC6Eg530NuD5Re7ZCZCJA32bzYG8Orjv2Q7nZC34ZC5n5jqjpkoZC6KbH0H7o5wcL2di5YqIiKt1aGxhLGA1hyjWjft4F8O8tcIly0nwKdBnlvsfJvaqZCcUMSv4ZD";
+const tokenu =
+  "EAAFrzBNW9DkBADycKZB7RpRKLt7NvMII3Iv96fDZCc2l5xF699gX8lG6uO1Pk5qZCNnck5195UOsManuIlicT2GBT2StwI6qRDbXiIpq3jzmNxKBQIavx5MM6UAPwa5pSZAz3MlMIX4q1WACwZBM5PN545Bu9OOWQkTglKd3L8Hh6PnT9sY6qnZB2AF9GVP5xuKMFLmdaXywCdIfth8TuZChWeoE8E3g55c3Cm4L3KMQvmpOuhHLF4BQ4PYo1ib9toZD";
+
+const tokenapp =
+  "EAAFrzBNW9DkBAPd8cx1Uzc2KWDTpzIWhWn1ZBHcRlQiBQLBiE819v1YaEHGsBcb0uEPdzmq6o06s0u16CBS7vDSZAFfxEMJe9RS3PvToXHaEYWsaIFslyUgQwUZB9gWRXWs67h1lXtQEEyhiFJZCoB7GwWBi9KZAdSki9flOUZB19WjA3c01ZBNFB4KZCND305cf8ytSbEHw5qgDZBCciukIrK06J8UPYGZCEZD";
 
 const IgFeed = () => {
-  return <InstagramFeed token={token} counter="6" />;
+  return <InstagramFeed token={tokenapp} counter="6" />;
 };
 
 export default IgFeed;
