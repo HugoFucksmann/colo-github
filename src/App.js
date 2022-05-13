6483d829484f05c82a0f6538a63386b235b4e72a
colo <hugoffuksmann@gmail.com>
2022-05-13 19:27:44 -0300

    probando snow y flyuptext, terminar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import appTheme from './theme/appTheme';
 import { ThemeProvider } from '@mui/system';
@@ -20,29 +19,6 @@ function App() {
 			</AppProvider>
 		</ThemeProvider>
 	);
-=======
-import "./App.css";
-import appTheme from "./theme/appTheme";
-import { ThemeProvider } from "@mui/system";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/index";
-import TreceProvider from "./treceweb/treceContext";
-import { CssBaseline } from "@mui/material";
-
-function App() {
-  return (
-    <ThemeProvider theme={appTheme}>
-      <CssBaseline />
-      <TreceProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </BrowserRouter>
-      </TreceProvider>
-    </ThemeProvider>
-  );
->>>>>>> d22789559860a842b1b4f5a77358cffc77414320
 }
 
 export default App;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import Snow from '../componets/backgroundAnimated/snow/snow';
+import FlyUpText from '../componets/textEffect/flyUpText/flyUpText';
 const Home = () => {
-	return <></>;
+	return (
+		<div style={{ backgroundColor: 'lightcoral', height: '100vh' }}>
+			hola
+		</div>
+	);
 };
 
 export default Home;
